2454a288fd0e8dfaab9d78307d5434b892534b5d
Alban Fraincart <afraincart@idsystemes.com>
2024-10-22 10:48:46 +0200

    Initial commit

---
+++ src/main/java/com/backend/projet3OC/Projet3OcApplication.java
@@ -0,0 +1,13 @@
+package com.backend.projet3OC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet3OcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet3OcApplication.class, args);
+	}
+
+}
